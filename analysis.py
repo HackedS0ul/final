cb473bd43b22ba21a41f1540a47d2d9be3918862
Administrator <virsila.modestas@gmail.com>
2024-01-26 14:37:20 +0200

    initial commit

---
+++ analysis.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+file_path = "namai.csv"
+products_df = pd.read_csv(file_path)
+
+products_df['Price'] = products_df['Price'].replace('N/A', pd.NA)
+products_df['Price'] = products_df['Price'].astype(str)
+
+products_df['Price'] = products_df['Price'].apply(lambda x: float(x) if pd.notna(x) and x else x)
+
+# print(products_df)
+# test
+
